2ffd5dd91024e2710066f2647f9092a40a84da32
AcHumphrey <AcHumphrey@example.com>
2019-08-11 18:12:37 +0100

    bugs fixed

--- app/src/main/java/com/example/bugfix0908/MainActivity.java
+++ app/src/main/java/com/example/bugfix0908/MainActivity.java
@@ -10,20 +10,25 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 private Button btnGoToActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //this line was missing
 
-
+        btnGoToActivity = findViewById(R.id.btn_go_to_activity); //initialization was missing
         btnGoToActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                goToNextActivity(); // this method call was missing
             }
         });
     }
 
     private void goToNextActivity(){
+
         Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent); //this line was missing
     }
+
 }
--- app/src/main/java/com/example/bugfix0908/SecondActivity.java
+++ app/src/main/java/com/example/bugfix0908/SecondActivity.java
@@ -3,12 +3,19 @@ package com.example.bugfix0908;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity { //was missing in the manifest
+
+    private TextView textView; // missing declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.tv_display); //missing initialization
+        textView.setVisibility(View.VISIBLE); //make view visible
     }
 }
